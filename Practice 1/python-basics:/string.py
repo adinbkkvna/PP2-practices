8227710ad14b7575a9cea1dcc8263f2259a7fc06
Adina <adina_kuralbek1@mail.ru>
2026-09-10 10:53:16 +0500

    Add Practice1 Python basics examples

---
+++ Practice 1/python-basics:/string.py
@@ -0,0 +1,19 @@
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+b = "Hello, World!"
+print(b[2:5])
+
+a = "Hello, World!"
+print(a.lower())
+print(a.upper())
+
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+b = "Hello, World!"
+print(b[-5:-2])
