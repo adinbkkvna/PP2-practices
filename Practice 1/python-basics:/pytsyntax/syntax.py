aab29cd3d626dc7e0014d4b68a3aa5f5efc3aef5
Adina <adina_kuralbek1@mail.ru>
2026-09-10 14:03:30 +0500

    Add Practice1 Python basics examples

--- Practice 1/python-basics:/pytsyntax/syntax.py
+++ Practice 1/python-basics:/pytsyntax/syntax.py
@@ -1,18 +1,22 @@
-if 5>2:
+if 5 > 2:
+  print("Five is greater than two!")
+
+#suntax error
+  if 5 > 2:
     print("Five is greater than two!")
 
 if 5 > 2:
  print("Five is greater than two!") 
 if 5 > 2:
-        print("Five is greater than two!")
-
-x = 5
-y = "Hello, World!"
-
-#This is a comment.
-print("Hello, World!")
+        print("Five is greater than two!") 
 
 #syntax error
 if 5 > 2:
  print("Five is greater than two!")
         print("Five is greater than two!")
+ 
+ x = 5
+y = "Hello, World!"
+
+#This is a comment.
+print("Hello, World!")
