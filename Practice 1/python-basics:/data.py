6b8ead5951ebd94c4478cc0ca5fb9323099da330
Adina <adina_kuralbek1@mail.ru>
2026-09-10 10:44:24 +0500

    Add Practice1 Python basics examples

---
+++ Practice 1/python-basics:/data.py
@@ -0,0 +1,20 @@
+x = 5
+print(type(x))
+
+x = ["apple", "banana", "cherry"]
+print(x)
+print(type(x)) 
+
+x = True
+print(type(x)) 
+
+
+x = memoryview(bytes(5))
+print(x)
+print(type(x)) 
+
+x = {"name" : "John", "age" : 36}
+
+print(x)
+print(type(x)) 
+
